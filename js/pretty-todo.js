f0949355c17647e70d63ea5b7c98b6e1b938e049
gayoungaa91 <gayoungaa91@gmail.com>
2019-06-28 15:07:27 +0900

    edit js/pretty-todo.js

---
+++ js/pretty-todo.js
@@ -0,0 +1,32 @@
+let todos = [
+  { id: 1, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: false },
+  { id: 3, content: 'Javascript', completed: false }
+]  
+
+const $ulList = document.querySelector('.todos');
+const $input = document.querySelector('.input');
+
+render();
+generateId();
+
+// 렌더링
+function render() {
+  $ulList.innerHTML = '';
+
+  todos.forEach ((todo) => { 
+   $ulList.innerHTML += `<li>${todo.content}</li>`
+  })
+}
+
+$input.addEventListener('keyup', (e) => {
+  if (e.keyCode !== 13) return;
+  todos = [...todos, { id: generateId(), content: e.target.value, completed: false }];
+  e.target.value = '';
+  render();
+});
+
+
+function generateId() {
+  return todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+}
